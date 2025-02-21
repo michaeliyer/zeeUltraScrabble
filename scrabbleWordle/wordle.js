ce8fc95a3b1a15694bf939749b406a70cb255b24
Michael Iyer <michaeliyer@Mac.lan>
2025-02-21 13:42:11 -0500

    Clove added and pulled outta there

--- scrabbleWordle/wordle.js
+++ scrabbleWordle/wordle.js
@@ -8066,6 +8066,12 @@ const oldWords = [
       myScore: 2,
       wordNumber: 1342,
     },
+    {
+      word: "CLOVE",
+      gameDate: "2/21/25",
+      myScore: 2,
+      wordNumber: 1343,
+    },
     
   ];
   // prettier-ignore
@@ -8090,7 +8096,7 @@ const oldWords = [
         'CHAIR', 'CHASE', 'CHASM', 'CHECK', 'CHESS', 'CHICK', 'CHIDE', 'CHILI', 'CHINA',
         'CHIRP', 'CHORE', 'CHUCK', 'CHUMP', 'CHURN', 'CIVIL', 'CLACK', 'CLAIM',
         'CLAMP', 'CLANG', 'CLANK', 'CLASH', 'CLASP', 'CLEAT', 'CLIFF', 'CLINK', 
-        'CLOUT', 'CLOVE', 'CLUED', 'CLUMP', 'CLUNG', 'COBRA', 'COLOR', 'COMFY', 'COMIC', 'CONCH',
+        'CLOUT', 'CLUED', 'CLUMP', 'CLUNG', 'COBRA', 'COLOR', 'COMFY', 'COMIC', 'CONCH',
         'COPSE', 'CORAL', 'COUCH', 'COUGH', 'COUPE', 'COVEN', 'COVEY', 'CRACK', 'CRASH',
         'CREAM', 'CREED', 'CREEK', 'CREEP', 'CREME', 'CRESS', 'CREST', 'CRICK',
         'CRIED', 'CRIER', 'CROCK', 'CRONY', 'CROUP', 'CRUDE',
