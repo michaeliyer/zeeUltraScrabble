1ff57ee7676360bb01fb1ca9aba8052c79f454a2
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-09-06 23:02:06 -0400

    Bulge dealt with, finally

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -12012,7 +12012,7 @@ console.log("Push, Stash, and Pop. What the Chryste!")
 console.log("August 4, 2025, Jackwagon. Dickerson. Eff On Off Guyo.")
 console.log("August 7, 2025, CORAL hits! Now what? Scion? Rinse? Caste? But CORAL HITS!")
 console.log("August 9, 2025, Mom: Hello" )
-console.log("September 6, 2025: Trump sux dykk0000! BULGE..")
+console.log("September 6, 2025: Trump sux dykk0000! BULGE")
 
 
 console.log(wordleWords)
