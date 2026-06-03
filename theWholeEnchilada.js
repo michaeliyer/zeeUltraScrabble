7b033e226115cc05183c4763a2679d521022ca1b
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-06-02 23:49:24 -0400

    Basis Dealt With

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -1591,7 +1591,7 @@ export const dailyWordsSmall = [
 'ATONE', 'ATRIA', 'AUDIO', 'AUDIT', 'AUGUR', 'AUNTY', 'AVAIL', 'AVERT', 'AVIAN',
 'AVOID', 'AWAIT', 'AWARD', 'AWARE', 'AWASH', 'AWFUL', 'AWOKE', 'AXIAL', 'AXIOM',
 'AXION', 'AZURE', 'BACON', 'BADGE', 'BADLY', 'BAGEL', 'BAGGY', 'BAKER', 'BALER', 'BALMY',
-'BALSA', 'BANAL', 'BANJO', 'BARGE', 'BARON', 'BASAL', 'BASIC', 'BASIN', 'BASIS',
+'BALSA', 'BANAL', 'BANJO', 'BARGE', 'BARON', 'BASAL', 'BASIC', 'BASIN', 
 'BASTE', 'BATCH', 'BATHE', 'BATON', 'BATTY', 'BAWDY', 'BAYOU', 'BEACH', 'BEADY', 'BEARD',
 'BEAST', 'BEAUT', 'BEECH', 'BEEFY', 'BEGAN', 'BEGAT', 'BEGET', 'BEGIN', 
 'BEING', 'BELCH', 'BELIE', 'BELLY', 'BELOW', 'BENCH', 'BERET', 'BERRY', 'BERTH',
@@ -1911,7 +1911,7 @@ export const dailyWordsLarge = [
     'BARDY', 'BARED', 'BARER', 'BARES', 'BARFI', 'BARFS', 'BARGE', 'BARIC', 'BARKS', 'BARKY',
     'BARMS', 'BARMY', 'BARNS', 'BARNY', 'BARON', 'BARPS', 'BARRA', 'BARRE', 'BARRO', 'BARRY',
     'BARYE', 'BASAL', 'BASAN', 'BASED', 'BASEN', 'BASER', 'BASES', 'BASHO', 'BASIC', 'BASIJ',
-    'BASIN', 'BASIS', 'BASKS', 'BASON', 'BASSE', 'BASSI', 'BASSO', 'BASSY', 'BASTA',
+    'BASIN', 'BASKS', 'BASON', 'BASSE', 'BASSI', 'BASSO', 'BASSY', 'BASTA',
     'BASTE', 'BASTI', 'BASTO', 'BASTS', 'BATCH', 'BATED', 'BATES', 'BATHE', 'BATHS', 'BATIK',
     'BATON', 'BATTA', 'BATTS', 'BATTU', 'BATTY', 'BAUDS', 'BAUKS', 'BAULK', 'BAURS', 'BAVIN',
     'BAWDS', 'BAWDY', 'BAWKS', 'BAWLS', 'BAWNS', 'BAWRS', 'BAWTY', 'BAYED', 'BAYER', 'BAYES',
@@ -24570,6 +24570,17 @@ export const wordleWords = [
         guess_5: null,
         guess_6: null,
       }, 
+      { word: "BASIS",
+        gameDate: "6/2/26",
+        myScore: 3,
+        wordNumber: 1809,
+        guess_1: "ROAST",
+        guess_2: "SCUBA",
+        guess_3: "BASIS",
+        guess_4: null,
+        guess_5: null,
+        guess_6: null,
+      }, 
 
  
   ];
@@ -24587,7 +24598,7 @@ console.log("Push, Stash, and Pop. What the Chryste!")
 console.log("August 4, 2025, Jackwagon. Dickerson. Eff On Off Guyo.")
 console.log("August 7, 2025, CORAL hits! Now what? Scion? Rinse? Caste? But CORAL HITS!")
 console.log("August 9, 2025, Mom: Hello" )
-console.log("June 1, 2026: Trump sux dykk000000000! CHILI")
+console.log("June 2, 2026: Trump sux dykk000000000! BASIS")
 
 console.log(wordleWords)
 
