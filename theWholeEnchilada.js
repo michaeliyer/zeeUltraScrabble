048d70f3731c78d15240a90ef22e1b8ae9b54f7f
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-07-24 23:35:17 -0400

    Putty Dealt With

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -1738,7 +1738,7 @@ export const dailyWordsSmall = [
 'PRIMO', 'PRIMP', 'PRINT', 'PRIOR', 'PRISM', 'PRIVY', 'PRIZE', 'PROBE', 'PRONE', 'PRONG',
 'PROOF', 'PROSE', 'PROUD', 'PROVE', 'PROWL', 'PROXY', 'PRUNE', 'PSALM', 'PUBIC',
 'PUDGY', 'PULPY', 'PULSE', 'PUNCH', 'PUPIL', 'PUREE', 'PURER', 'PURGE',
-'PURSE', 'PUSHY', 'PUTTY', 'PYGMY', 'PYLON', 'QUAIL', 'QUAKE', 'QUALM', 'QUARK', 
+'PURSE', 'PUSHY', 'PYGMY', 'PYLON', 'QUAIL', 'QUAKE', 'QUALM', 'QUARK', 
 'QUART',
 'QUASH', 'QUASI', 'QUEEN', 'QUERY', 'QUEST', 'QUEUE', 'QUICK', 'QUIET',
 'QUILL', 'QUILT', 'QUIRK', 'QUITE', 'QUOTA', 'QUOTE', 'QUOTH', 'RABBI', 'RABID', 'RACER',
@@ -2712,7 +2712,7 @@ export const dailyWordsLarge = [
     'PUNGA', 'PUNGS', 'PUNJI', 'PUNKA', 'PUNKS', 'PUNKY', 'PUNNY', 'PUNTO', 'PUNTS', 'PUNTY',
     'PUPAE', 'PUPAL', 'PUPAS', 'PUPIL', 'PUPUS', 'PURDA', 'PURED', 'PUREE', 'PURER',
     'PURES', 'PURGE', 'PURIN', 'PURIS', 'PURLS', 'PURPY', 'PURRS', 'PURSE', 'PURSY', 'PURTY',
-    'PUSES', 'PUSHY', 'PUSLE', 'PUSSY', 'PUTID', 'PUTON', 'PUTTI', 'PUTTO', 'PUTTS', 'PUTTY',
+    'PUSES', 'PUSHY', 'PUSLE', 'PUSSY', 'PUTID', 'PUTON', 'PUTTI', 'PUTTO', 'PUTTS',
     'PUZEL', 'PWNED', 'PYATS', 'PYETS', 'PYGAL', 'PYGMY', 'PYINS', 'PYLON', 'PYNED', 'PYNES',
     'PYOID', 'PYOTS', 'PYRAL', 'PYRAN', 'PYRES', 'PYREX', 'PYRIC', 'PYROS', 'PYXED', 'PYXES',
     'PYXIE', 'PYXIS', 'PZAZZ',
@@ -25150,6 +25150,17 @@ export const wordleWords = [
         guess_5: null,
         guess_6: null,
       }, 
+      { word: "PUTTY", 
+        gameDate: "7/24/26",
+        myScore: 3,
+        wordNumber: 1861,
+        guess_1: "LEANT",
+        guess_2: "STRIP",
+        guess_3: "PUTTY",
+        guess_4: null,
+        guess_5: null,
+        guess_6: null,
+      }, 
       
 
  
@@ -25168,7 +25179,7 @@ console.log("Push, Stash, and Pop. What the Chryste!")
 console.log("August 4, 2025, Jackwagon. Dickerson. Eff On Off Guyo.")
 console.log("August 7, 2025, CORAL hits! Now what? Scion? Rinse? Caste? But CORAL HITS!")
 console.log("August 9, 2025, Mom: Hello" )
-console.log("July 23, 2026: Trump sux dykk000000000! ORBIT")
+console.log("July 24, 2026: Trump sux dykk000000000! PUTTY")
 
 console.log(wordleWords)
 
